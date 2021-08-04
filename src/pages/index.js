6005cb961ad8f6ada39132dbfce0462fe424255a
emaulandi <e.maulandi@gmail.com>
2021-08-04 15:32:40 +0200

    Clean test content

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,6 @@ const HomeIndex = () => {
         </section>
 
         <section id="one" className="tiles">
-
           {homeContentCsv.map(({ title, subtitle, slug }, index) => {
             return (
               <article key={`article-${index}`} style={{backgroundImage: `url(${backgroundPics[index]})`}}>
@@ -74,21 +73,7 @@ const HomeIndex = () => {
           })}
 
         </section>
-
-        <section id="two">
-          <div className="inner">
-            <header className="major">
-              <h2>Massa libero</h2>
-            </header>
-            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
-            <ul className="actions">
-              <li><Link to="/landing" className="button next">Get Started</Link></li>
-            </ul>
-          </div>
-        </section>
-
       </div>
-
     </Layout>
   )
 }
