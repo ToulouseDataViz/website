473cb3a97d77d92e2e812de56364d727e113cfc1
emaulandi <e.maulandi@gmail.com>
2021-08-20 14:53:42 +0200

    Update external resources links

--- src/settings.js
+++ src/settings.js
@@ -9,3 +9,4 @@ export const mailLink = 'mailto:contact@toulouse-dataviz.fr';
 export const twitterLink = 'https://twitter.com/tls_dataviz?lang=fr';
 export const youtubeLink = 'https://www.youtube.com/channel/UCo64gnxLZs1xIN-Y0Bv_Hpg/about';
 export const meetupLink = 'https://www.meetup.com/fr-FR/Meetup-Visualisation-des-donnees-Toulouse/';
+export const clubInscriptionLink = 'https://eepurl.com/g8tVZL';
