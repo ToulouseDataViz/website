02fe090923531d6a2c65ae9f1d4fe6a8627ccd64
Alain Roan <alain.roan@gmail.com>
2021-09-11 11:01:44 +0200

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,8 @@
 export const pastEventStatusName = 'Réalisé';
 export const incomingEventStatusName = 'Planifié';
 
-export const newslettersUrl = 'https://newsletters.toulouse-dataviz.fr/#e239c954d9a04780843e173559717360';
-export const collectionsUrl = 'https://newsletters.toulouse-dataviz.fr/#b433978536e5497cab923c3db499afb5';
+export const newslettersUrl = 'https://ressources.toulouse-dataviz.fr/';
+export const collectionsUrl = 'https://ressources.toulouse-dataviz.fr/collections';
 
 export const discordLink = 'https://discord.gg/Ch23qScbpc';
 export const mailLink = 'mailto:contact@toulouse-dataviz.fr';
