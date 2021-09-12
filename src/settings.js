2163e92ea613640010d5500d48283bfd2b8a9669
Vincent VIVANLOC <vincent.vivanloc@laposte.net>
2021-09-12 13:48:18 +0200

    add: (footer) github link add: (footer) shuffle to be compliant with resources homepage add: (footer) legacy site

--- src/settings.js
+++ src/settings.js
@@ -7,6 +7,8 @@ export const collectionsUrl = 'https://ressources.toulouse-dataviz.fr/collection
 export const discordLink = 'https://discord.gg/Ch23qScbpc';
 export const mailLink = 'mailto:contact@toulouse-dataviz.fr';
 export const twitterLink = 'https://twitter.com/tls_dataviz?lang=fr';
+export const gitHubLink = 'https://github.com/ToulouseDataViz';
 export const youtubeLink = 'https://www.youtube.com/channel/UCo64gnxLZs1xIN-Y0Bv_Hpg/about';
 export const meetupLink = 'https://www.meetup.com/fr-FR/Meetup-Visualisation-des-donnees-Toulouse/';
+export const websiteLegacyLink  = 'https://toulouse-dataviz.fr/';
 export const clubInscriptionLink = 'https://eepurl.com/g8tVZL';
