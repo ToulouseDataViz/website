e65e59b39d98ed7d091bdccbfa526c2e85465311
Vincent VIVANLOC <vincent.vivanloc@laposte.net>
2022-02-06 12:59:53 +0100

    fix: (sponsors) remove dead code

--- src/components/HackavizSponsors.js
+++ src/components/HackavizSponsors.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage } from 'gatsby-plugin-image';
 
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -12,31 +12,27 @@ import { getPic } from '../helper';
 const useStyles = makeStyles(theme => ({
   imageGridContainer: {
     backgroundColor: 'white',
-    padding: theme.spacing(1,0),
+    padding: theme.spacing(1, 0),
   },
   imageContainer: {
-    maxWidth: '150px',    
+    maxWidth: '150px',
   },
   gatsbyImageCentered: {
-    verticalAlign: 'middle'
-  }
+    verticalAlign: 'middle',
+  },
 }));
 
 const HackavizSponsors = ({ currentHackaviz }) => {
   const classes = useStyles();
   let hackavizSponsors = useHackavizSponsors();
-  if (currentHackaviz!==null) { 
-    hackavizSponsors = hackavizSponsors.filter(({ hackaviz }) => hackaviz === currentHackaviz);
-  } else {
-    let hackavizSponsorsKeys=hackavizSponsors.map(({ sponsor_pic }) => sponsor_pic );
-    hackavizSponsorsKeys = [...new Set(hackavizSponsorsKeys)];
-    hackavizSponsors=hackavizSponsorsKeys.map( sponsor_pic  => {return { sponsor_pic }});
-  }
+  hackavizSponsors = hackavizSponsors.filter(
+    ({ hackaviz }) => currentHackaviz === null || hackaviz === currentHackaviz
+  );
   const sponsorsPics = usePics().filter(({ relativeDirectory }) => relativeDirectory === 'sponsor-pics');
 
   return (
-    <Grid 
-      container 
+    <Grid
+      container
       spacing={2}
       direction="row"
       justify="space-around"
@@ -47,13 +43,12 @@ const HackavizSponsors = ({ currentHackaviz }) => {
         const sponsorPic = getPic(sponsorsPics, sponsor_pic);
         return (
           <Grid item className={classes.imageContainer}>
-            <GatsbyImage image={sponsorPic} alt={sponsor_pic} className={classes.gatsbyImageCentered}/>
+            <GatsbyImage image={sponsorPic} alt={sponsor_pic} className={classes.gatsbyImageCentered} />
           </Grid>
-          
         );
       })}
     </Grid>
   );
-}
+};
 
-export default HackavizSponsors
+export default HackavizSponsors;
