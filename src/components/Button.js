fb7c40cd0ef2c007046e73d3c62b31b1952c8f66
Vincent VIVANLOC <vincent.vivanloc@laposte.net>
2022-05-15 00:49:04 +0200

    fix: (button) remove 'special' button to improve readability

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,31 +4,25 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 
 const externalLinkProps = {
-  target: "_blank",
-  rel: "noreferrer",
+  target: '_blank',
+  rel: 'noreferrer',
 };
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1, 2, 1, 0),
-  }
+  },
 }));
 
-const Button = ({ link, text, size = "medium", type = 'external', display = '' }) => {
+const Button = ({ link, text, size = 'medium', type = 'external', display = '', style = undefined }) => {
   const classes = useStyles();
-  const linkProps = type === "external"
-   ? externalLinkProps
-   : null;
+  const linkProps = type === 'external' ? externalLinkProps : null;
 
   return (
-    <a 
-      href={link}
-      className={clsx(["button"], [size], [classes.button], [display])}
-      { ...linkProps} 
-    >
+    <a href={link} className={clsx(['button'], [size], [classes.button], [display])} style={style} {...linkProps}>
       {text}
     </a>
   );
-}
+};
 
-export default Button
+export default Button;
