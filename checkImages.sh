fbeadc4eb61df9253a0c7894af8523ac106bda63
Vincent VIVANLOC <vincent.vivanloc@laposte.net>
2023-04-01 22:05:04 +0200

    add arg

--- checkImages.sh
+++ checkImages.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 # adapted from http://unix.stackexchange.com/a/157594/110635
 # and http://unix.stackexchange.com/a/220619/110635
-W=1024
-H=768
+W=1280
+H=800
 SIZE_TEST="%[fx:(h>$H && w>$W)]"'\n'
 
-#echo $files
+while test $# -gt 0
+do
+    case "$1" in
+        -c) 
+            echo "convert files!"
+            convert=true
+            ;;
+        *) echo "-c to convert"
+            exit 0
+            ;;
+    esac
+    shift
+done
+
 find src static -type f \( -name \*.png -o -name \*.jp* -o -name \*.webm \) | while read f; do 
    if [ $(identify -format "$SIZE_TEST" "$f") = 1 ]; then
-   echo "Should be resized: $f"
-    #mogrify -resize ''"$W"x"$H"'' "$f"
-  #else
-    #echo "Do not resize: $f"
+   file_size_kb=`du -k "$f" | cut -f1`
+   file_info=$(identify -format "%wx%h\n" "$f")
+   echo "Will be resized: $f ($file_size_kb kB) $file_info"
+   if [[ convert ]]
+   then
+    mogrify -resize ''"$W"x"$H"'' "$f"
+   fi
   fi
 done
   
